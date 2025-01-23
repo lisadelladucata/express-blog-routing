564e3ecbab84900330b29ec8703a10a29b510ae5
lisadelladucata <lisa.delladucata@gmail.com>
2025-01-24 00:26:19 +0100

    feat:add-bonus

--- routers/posts.js
+++ routers/posts.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const blogs = require('../data/blog')
+
+
 const router = express.Router()
 
 //index
 router.get('/' , (req, res) =>{
-    res.send('lista dei post')
+    res.json(blogs)
 })
 
 // show
 router.get('/:id', (req, res) =>{
-    res.send(` dettagli del post: ${req.params.id}` )
+    if(isNaN(req.params.id)){
+        return res.sendStatus(400)
+    }    
+
+    const post = blogs.find((elm) => elm.id == req.params.id)
+    if(!post){
+        res.sendStatus(404)
+    }
+    res.json(post)
 })
 
 // create
